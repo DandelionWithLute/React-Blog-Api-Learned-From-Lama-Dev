04aab38f47194aa41d9ed553bb04c2b6382c266b
OrangeBirdDrinksWater <114991717+OrangeBirdDrinksWater@users.noreply.github.com>
2023-01-29 01:01:49 +0800

    v0.3
    v0.3
    41min41sec

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const dotenv = require("dotenv")//not required
 const mongoose = require("mongoose")
 const authRoute = require("./routes/auth")
+const userRoute = require("./routes/users")
 /*
 "scripts": {
     "start": "nodemon index.js"
@@ -26,6 +27,7 @@ mongoose.connect(process.env.MONGO_URL, {//Don't change this to the next line be
 
 
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 app.listen("5000", () => {
     console.log("Backend is running.")
